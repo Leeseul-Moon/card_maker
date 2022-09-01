1e288b7b1a91633170b6a77f2e720aed2075d83c
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-09-01 14:32:25 +0900

    remove warnings + firebase clean up

--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -1,6 +1,6 @@
 // import { getDatabase, ref, set, remove } from "firebase/database";
 
-import firebaseApp from "./firebase";
+import { firebaseDatabase } from "./firebase";
 
 // class CardRepository {
 //   saveCard(userId, card) {
@@ -17,7 +17,7 @@ import firebaseApp from "./firebase";
 
 class CardRepository {
   syncCards(userId, onUpdate) {
-    const ref = firebaseApp.database().ref(`${userId}/cards`);
+    const ref = firebaseDatabase.ref(`${userId}/cards`);
     ref.on("value", (snapshot) => {
       const value = snapshot.val();
       value && onUpdate(value);
@@ -26,11 +26,11 @@ class CardRepository {
   }
 
   saveCard(userId, card) {
-    firebaseApp.database().ref(`${userId}/cards/${card.id}`).set(card);
+    firebaseDatabase.ref(`${userId}/cards/${card.id}`).set(card);
   }
 
   removeCard(userId, card) {
-    firebaseApp.database().ref(`${userId}/cards/${card.id}`).remove();
+    firebaseDatabase.ref(`${userId}/cards/${card.id}`).remove();
   }
 }
 
